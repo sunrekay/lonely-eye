4f81739023485f35c402a5c5c716467b37a781ef
surendoy <wladkorostilev@yandex.ru>
2024-03-06 17:33:53 +0300

    Change longitude and latitude from str type to pydantic _extra_types Longitude and Latitude

--- lonely_eye/cameras/schemas.py
+++ lonely_eye/cameras/schemas.py
@@ -1,14 +1,21 @@
 import uuid
+from typing import Annotated
 
+from annotated_types import MaxLen, Gt, Lt
 from pydantic import BaseModel
+from pydantic_extra_types.coordinate import Longitude, Latitude
 
 
 class CameraRegistration(BaseModel):
-    type: int
-    longitude: str
-    latitude: str
-    description: str
+    type: Annotated[int, Gt(0), Lt(40)]
+    longitude: Longitude
+    latitude: Latitude
+    description: Annotated[str, MaxLen(750)]
 
 
 class CamRegRes(CameraRegistration):
     key: uuid.UUID
+
+
+class UploadCase(BaseModel):
+    data: str
