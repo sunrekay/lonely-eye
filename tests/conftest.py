97ada45c3aeb027c766223155e5379090cbf74d3
surendoy <wladkorostilev@yandex.ru>
2024-03-12 20:41:44 +0300

    Pytest config

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import os
+from typing import AsyncGenerator
+
+import dotenv
+import pytest
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+from lonely_eye.database import database
+from lonely_eye.models import Base
+from lonely_eye.main import app
+
+dotenv.load_dotenv()
+
+DB_HOST_TEST = os.getenv("DB_HOST_TEST")
+DB_PORT_TEST = os.getenv("DB_PORT_TEST")
+DB_NAME_TEST = os.getenv("DB_NAME_TEST")
+DB_USER_TEST = os.getenv("DB_USER_TEST")
+DB_PASS_TEST = os.getenv("DB_PASS_TEST")
+
+DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+
+engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+async_session_maker = sessionmaker(
+    engine_test, class_=AsyncSession, expire_on_commit=False
+)
+Base.metadata.bind = engine_test
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[database.session_dependency] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url="http://test/api/v1") as ac:
+        yield ac
