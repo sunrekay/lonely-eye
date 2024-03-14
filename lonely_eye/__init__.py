3ab77b3ef5528ab71701cf25bf57ca760dbd2a70
surendoy <wladkorostilev@yandex.ru>
2024-03-14 17:23:29 +0300

    Add Violation model for alembic import

--- lonely_eye/__init__.py
+++ lonely_eye/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "Camera",
     "CarOwner",
     "Transport",
+    "Violation",
 )
 
 from lonely_eye.cameras.models import Camera
 from lonely_eye.cars_owners.models import CarOwner, Transport
+from lonely_eye.violations.models import Violation
