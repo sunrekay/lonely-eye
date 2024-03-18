834839b2422e452388b44bf1cfc6bccf193de589
surendoy <wladkorostilev@yandex.ru>
2024-03-18 12:34:23 +0300

    Add Redis fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
+import asyncio
 import os
 from typing import AsyncGenerator
 
 import dotenv
 import pytest
 from httpx import AsyncClient
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
+
 
 from lonely_eye.database import database
 from lonely_eye.models import Base
@@ -24,7 +27,9 @@ DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOS
 
 engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
 async_session_maker = sessionmaker(
-    engine_test, class_=AsyncSession, expire_on_commit=False
+    engine_test,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
 Base.metadata.bind = engine_test
 
@@ -50,3 +55,20 @@ async def prepare_database():
 async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test/api/v1") as ac:
         yield ac
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+async def redis(event_loop):
+    redis = Redis(host="localhost", port=6379, db=0, decode_responses=True)
+    yield redis
+    await redis.aclose()
