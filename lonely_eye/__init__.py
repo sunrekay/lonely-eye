63b3abe1ecb0dfe655af4db24d06320717d57e89
surendoy <wladkorostilev@yandex.ru>
2024-03-19 18:31:59 +0300

    Add model Case for alembic

--- lonely_eye/__init__.py
+++ lonely_eye/__init__.py
@@ -1,7 +1,16 @@
-__all__ = ("Camera", "CarOwner", "Transport", "Violation", "Worker", "Manager")
+__all__ = (
+    "Camera",
+    "CarOwner",
+    "Transport",
+    "Violation",
+    "Worker",
+    "Manager",
+    "Case",
+)
 
 from lonely_eye.cameras.models import Camera
 from lonely_eye.cars_owners.models import CarOwner, Transport
 from lonely_eye.violations.models import Violation
 from lonely_eye.workers.models import Worker
 from lonely_eye.managers.models import Manager
+from lonely_eye.cases.models import Case
