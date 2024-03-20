bcd555da3185009a225bdf2e5bba4c05bcbc7f78
surendoy <wladkorostilev@yandex.ru>
2024-03-20 09:30:25 +0300

    Fix config

--- lonely_eye/config.py
+++ lonely_eye/config.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).parent.parent
 
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
