196478731199e962068824bc055e2b9da020053b
surendoy <wladkorostilev@yandex.ru>
2024-03-20 06:23:23 +0300

    Test data for cases package

---
+++ tests/cases/__init__.py
@@ -0,0 +1,10 @@
+worker1: dict = {
+    "username": "Vladimir",
+    "email": "vladimir@mail.com",
+    "password": "vladimir12345",
+}
+worker2: dict = {
+    "username": "Domitry",
+    "email": "domytrus@mail.com",
+    "password": "domitrus54321",
+}
